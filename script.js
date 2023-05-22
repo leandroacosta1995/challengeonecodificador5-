5ff5a5f6c31f0626eefbaeea759d01f0de71e055
Leandro <nando8569@gmail.com>
2023-05-22 09:38:12 -0300

    Encriptador Alura: Grupo 5

---
+++ script.js
@@ -0,0 +1,49 @@
+// e---> enter
+// o---> ober
+// i---> imes
+// a---> ai
+// u---> ufat
+
+//i g m : la i para mayusculas y minisculas, g para toda la linea u oracion, m afeta a multiples lineas
+
+function encriptar(){
+    
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/img,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+    
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/img,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+
+function copiar(){
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+
+}
